ed9a30cd859a311ef57b2035686326a971501813
Sasho <sasoh@Alexanders-Mac.local>
2015-02-08 20:14:53 +0200

    Adding initial Xcode project

---
+++ White collar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  White collar
+//
+//  Created by Alexander Popov on 2/8/15.
+//  Copyright (c) 2015 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
